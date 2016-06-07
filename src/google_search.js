7436c68297a87e2814cf762321951c865f10e8da
lolipuerta <loliymanolo30>
2016-06-07 18:54:06 +0200

    Improve code Signed-off-by: lolipuerta <loliymanolo30>

--- src/google_search.js
+++ src/google_search.js
@@ -44,7 +44,7 @@ var driver = new webdriver.Builder()
     .build();
 
 driver.get('http://www.google.com/ncr');
-driver.findElement(By.id('q')).sendKeys('webdriver');
+driver.findElement(By.id('lst-ib')).sendKeys('webdriver');
 driver.findElement(By.name('btnG')).click();
 driver.wait(until.titleIs('webdriver - Google Search'), 1000);
 driver.sleep(10000);
